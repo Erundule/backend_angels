72565192a0a36cf17f71058998e5aa31be1dd82b
rikcav <henriquearau000@gmail.com>
2024-06-08 13:32:41 -0300

    feat: dados evolutivos routes added to project routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { gestanteRoutes } from "./gestante/routes";
+import { dadosEvolutivosRoutes } from "./dados_evolutivos/routes";
 
 module.exports = (app: express.Application) => {
   gestanteRoutes(app);
+  dadosEvolutivosRoutes(app);
 };
